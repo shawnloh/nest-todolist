0adf598ebc38df4f41524d887350d99e4db2f33c
Shawn Loh <shawnlohb@gmail.com>
2021-03-13 20:05:52 +0800

    Changed name of LoggingInterceptor to RequestLoggingInterceptor, clean up of unused variables. Change name LoggingInterceptor variable to RequestLoggingInterceptor, configure to use ormconfig.js config for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { LoggingInterceptor } from './shared/interceptors/logging.interceptor';
+import { RequestLoggingInterceptor } from './shared/interceptors/logging.interceptor';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getConnectionOptions } from 'typeorm';
-import { resolve } from 'path';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -15,27 +15,19 @@ import { resolve } from 'path';
       cache: true,
     }),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
+      useFactory: async () => {
         return Object.assign(await getConnectionOptions(), {
           autoLoadEntities: true,
-          logging: true,
-          type: configService.get('DB_TYPE'),
-          host: configService.get('DB_HOST'),
-          port: configService.get('DB_PORT'),
-          username: configService.get('DB_USERNAME'),
-          password: configService.get('DB_PASSWORD'),
-          database: configService.get('DB_DATABASE'),
         });
       },
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [
     {
       provide: APP_INTERCEPTOR,
-      useClass: LoggingInterceptor,
+      useClass: RequestLoggingInterceptor,
     },
   ],
 })
